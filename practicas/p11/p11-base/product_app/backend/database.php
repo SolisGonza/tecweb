6a5951185090cd52ac187effc2df4897ae6abcc5
Gonzalo <gonzalo.tobon@alumno.buap.mx>
2024-10-14 15:46:01 -0600

    Busqueda de productos: Final

--- practicas/p11/p11-base/product_app/backend/database.php
+++ practicas/p11/p11-base/product_app/backend/database.php
@@ -1,15 +1,13 @@
 <?php
     $conexion = @mysqli_connect(
-        'localhost',
-        'root',
-        '12345678a',
-        'marketzone'
+        'localhost', 'root', '1001', 'marketzone'
     );
 
     /**
      * NOTA: si la conexión falló $conexion contendrá false
      **/
     if(!$conexion) {
-        die('¡Base de datos NO conextada!');
+        die('¡Base de datos NO conectada!');
     }
 ?>
+
--- practicas/p11/p11-base/product_app/backend/read.php
+++ practicas/p11/p11-base/product_app/backend/read.php
@@ -27,3 +27,4 @@
     // SE HACE LA CONVERSIÓN DE ARRAY A JSON
     echo json_encode($data, JSON_PRETTY_PRINT);
 ?>
+
