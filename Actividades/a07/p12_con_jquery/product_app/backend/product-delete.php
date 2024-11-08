52304e6d62250574116f73aebdf44aef358a21df
Gonzalo <gonzalo.tobon@alumno.buap.mx>
2024-11-07 21:49:45 -0600

    Final

--- Actividades/a07/p12_con_jquery/product_app/backend/product-delete.php
+++ Actividades/a07/p12_con_jquery/product_app/backend/product-delete.php
@@ -1,30 +1,5 @@
 <?php
-    /*
-    include_once __DIR__.'\myapi\Products.php';
-
-    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
-    $data = array(
-        'status'  => 'error',
-        'message' => 'La consulta falló'
-    );
-    // SE VERIFICA HABER RECIBIDO EL ID
-    if( isset($_GET['id']) ) {
-        $id = $_GET['id'];
-        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-        $sql = "UPDATE productos SET eliminado=1 WHERE id = {$id}";
-        if ( $conexion->query($sql) ) {
-            $data['status'] =  "success";
-            $data['message'] =  "Producto eliminado";
-		} else {
-            $data['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($conexion);
-        }
-		$conexion->close();
-    } 
-    
-    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
-    echo json_encode($data, JSON_PRETTY_PRINT);
-    */
-
+  
     namespace product_app\backend;
     use product_app\backend\myapi\Products;
     require_once __DIR__ . '/myapi/Products.php';
--- Actividades/a07/p12_con_jquery/product_app/backend/product-search-name.php
+++ Actividades/a07/p12_con_jquery/product_app/backend/product-search-name.php
@@ -1,21 +1,5 @@
 <?php
-/*
-// product-search-name.php
-include_once __DIR__.'/database.php';
 
-$data = json_decode(file_get_contents('php://input'), true);
-$nombre = $data['nombre'] ?? '';
-
-if (empty($nombre)) {
-    echo json_encode(['error' => 'Nombre no proporcionado']);
-    exit;
-}
-
-$result = mysqli_query($conexion, "SELECT COUNT(*) FROM productos WHERE nombre = '$nombre'");
-$exists = $result ? mysqli_fetch_array($result)[0] > 0 : false;
-
-echo json_encode(['exists' => $exists]);
-*/
 namespace product_app\backend;
 use product_app\backend\myapi\Products;
 require_once __DIR__ . '/myapi/Products.php';
