0f4a0c1d4dca01b4b98ea15d27eac09fa31ea6ae
Gonzalo <gonzalo.tobon@alumno.buap.mx>
2024-11-30 20:56:55 -0600

    Borrado de archivos PHP

--- practicas/p18/psr-4/backend/product-delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-  
-    use Gonza\P13\Delete\Delete as Delete; 
-    require_once __DIR__ . '/../vendor/autoload.php';
-    $productos = new Delete('marketzone');
-    $productos->delet($_GET['id']);  
-    echo $productos->getData(); 
-?> 
--- practicas/p18/psr-4/backend/product-search.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    use Gonza\P13\Read\Read as Read; 
-    require_once __DIR__ . '/../vendor/autoload.php';
-    $productos = new Read('marketzone');
-    $productos->search($_GET['search']);  
-    echo $productos->getData();  
-
-?>
